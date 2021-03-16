76a5203f74267a052bc873e6aa6173659cb43f06
jadenandrews831 <jadenandrews831@gmail.com>
2021-03-16 14:56:12 -0400

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Jaden  Andrews  on 3/16/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
